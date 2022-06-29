e9ae21e00f15ca47e07c971519da64adec6c261b
Ridercogswell <ridercogswell@gmail.com>
2022-06-28 22:34:03 -0700

    BST

---
+++ tree.js
@@ -0,0 +1,32 @@
+// class BST
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+// iterative approach
+const findClosestValueInBST = (tree, target) => {
+  return findClosestHelper(tree, target, tree.value)
+}
+
+const findClosestHelper = (tree, target, closest) => {
+  let currentNode = tree;
+
+  while (currentNode !== null) {
+    if (Math.abs(target - closest) > Math.abs(target - currentNode.value)) {
+      closest = currentNode.value;
+    }
+
+    if (target < currentNode.value) {
+      currentNode = currentNode.left;
+    } else if (target > currentNode.value) {
+      currentNode = currentNode.right;
+    } else {
+      break;
+    }
+  }
+  return closest;
+}
