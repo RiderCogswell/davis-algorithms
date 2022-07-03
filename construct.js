d4cb93d17a460b72675231786cd646360a4d8497
Ridercogswell <ridercogswell@gmail.com>
2022-07-02 19:00:36 -0700

    bst insert

--- construct.js
+++ construct.js
@@ -2,16 +2,28 @@ class BST {
   constructor(value) {
     this.value = value;
     this.left = null;
-    this.righ = null;
+    this.right = null;
   }
 
+  // recursive
   insert(value) {
+    // if value is greater than current value, work on left side
     if (value > this.value) {
 
+      if (this.left === null) {
+        this.left = new BST(value)
+      } else {
+        this.left.insert(value)
+      }
+      // else, work on right
     } else {
       if (this.right === null) {
-        this.right = 
-      }
+        this.right = new BST(value)
+      } else {
+        this.right.insert(value)
+      } 
+      
+    return this;
     }
   }
 }
