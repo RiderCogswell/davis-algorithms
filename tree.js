eca1f9f3d55d21c5a65f6772e0ae0a3c84631690
Ridercogswell <ridercogswell@gmail.com>
2022-07-06 21:54:43 -0700

    post google

--- tree.js
+++ tree.js
@@ -128,3 +128,34 @@ const swapLeftAndRight = (tree) => {
   // set right = left variable!
   tree.right = left;
 }
+// iterative | O(height) T | O(1) S
+class BinaryTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+    this.parent = null;
+  }
+}
+
+function findSuccessor(tree, node) {
+  if (node.right !== null) return getLeftMostChild(node.right);
+
+  return getRightMostParent(node);
+}
+
+function getLeftMostChild(node) {
+  let currentNode = node;
+  while (currentNode.left !== null) {
+    currentNode = currentNode.left;
+  }
+  return currentNode
+}
+
+function getRightMostParent(node) {
+  let currentNode = node;
+  while (currentNode.parent !== null && currentNode.parent.right === currentNode) {
+    currentNode = currentNode.parent
+  }
+  return currentNode.parent;
+}
