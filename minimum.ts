009e3c5a773f5a0d0e386e573dbefdcaa614e0d9
Ridercogswell <ridercogswell@gmail.com>
2022-07-26 19:19:44 -0700

    minimumWaitingTime

---
+++ minimum.ts
@@ -0,0 +1,13 @@
+function minimumWaitingTime(queries: number[]): number {
+  queries.sort((a, b) => a - b);
+
+  let totalTime = 0;
+
+  for (let i = 0; i < queries.length; i++) {
+    const duration = queries[i];
+    const queriesLeft = queries.length - (i + 1);
+    totalTime += duration * queriesLeft;
+  }
+
+  return totalTime;
+}
