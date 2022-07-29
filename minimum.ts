ea9bcdca9c7c1ea339e489ffddf7d7dd5dc25421
Ridercogswell <ridercogswell@gmail.com>
2022-07-28 17:21:03 -0700

    comment minwaitingtime

--- minimum.ts
+++ minimum.ts
@@ -1,11 +1,11 @@
-function minimumWaitingTime(queries: number[]): number {
-  queries.sort((a, b) => a - b);
+export function minimumWaitingTime(queries: number[]): number {
+  queries.sort((a, b) => a - b);  // sort through array
 
-  let totalTime = 0;
+  let totalTime = 0; // declare a count variable
 
-  for (let i = 0; i < queries.length; i++) {
-    const duration = queries[i];
-    const queriesLeft = queries.length - (i + 1);
+  for (let i = 0; i < queries.length; i++) { // loop through array
+    const duration = queries[i]; // create a variable of iteration
+    const queriesLeft = queries.length - (i + 1); // create a variable that keeps track of how many queries are left
     totalTime += duration * queriesLeft;
   }
 
@@ -13,10 +13,10 @@ function minimumWaitingTime(queries: number[]): number {
 }
 
 export function nonCunstructibleChange(coins: number[]): number {
-  coins.sort((a, b) => a - b);
+  coins.sort((a, b) => a - b);  // sort through array
 
-  let currentChange = 0;
-  for (const coin of coins) {
+  let currentChange = 0; // declare a count variable
+  for (const coin of coins) { // loop through array
     if (coin > currentChange + 1) return currentChange + 1;
     currentChange += coin
   }
