93f0ead2f0e211e2ad2c517ddc283f798ba56ddd
Ridercogswell <ridercogswell@gmail.com>
2022-08-20 14:37:36 -0700

    string time conversion

---
+++ time.js
@@ -0,0 +1,21 @@
+function timeConversion(s) { // s = '1:00:05PM'
+  // Write your code here
+  let amPm = s.charAt(8); // find the 'A' || 'P' letter
+  let milTime = '';
+  
+  if (amPm === 'A') {
+      if (s.substring(0, 2) === '12') { // if
+          milTime = '00'
+      } else {
+          milTime = s.substring(0, 2)
+      }
+  } else { // 'P'
+      if (s.substring(0, 2) === '12') {
+          milTime = s.substring(0, 2);
+      } else {
+          milTime = parseInt(s.substring(0, 2), 10) + 12;
+      }
+  }
+  
+  return (milTime + s.substring(2, 8))
+} 
