16c3d2bc21cc94d9effbfd93a40d114eb4e57c3b
Ridercogswell <ridercogswell@gmail.com>
2022-10-11 13:49:48 -0700

    begin repeating

--- strings.ts
+++ strings.ts
@@ -1,26 +1,40 @@
-const vowelsNum = (str: string): number => {
-  let count = 0;
+// const vowelsNum = (str: string): number => {
+//   let count = 0;
 
-  for (let i = 0; i < str.length; i++) {
-    let val = str[i].toLowerCase()
-    if (val === 'a' || val === 'i' || val === 'u' || val === 'e' || val === 'o') {
-      count++
+//   for (let i = 0; i < str.length; i++) {
+//     let val = str[i].toLowerCase()
+//     if (val === 'a' || val === 'i' || val === 'u' || val === 'e' || val === 'o') {
+//       count++
+//     }
+//   }
+
+//   return count;
+// }
+
+// vowelsNum('AAAAUUAUAUAUAUUAUAUAUA')
+
+// const palindromeCheck = (str: string): boolean => {
+//   let reversed = str.split('').reverse().join('')
+
+//   if (reversed === str) {
+//     return true;
+//   }
+
+//   return false;
+// }
+
+// palindromeCheck('racecar')
+
+const repeatingChar = (string: string): string {
+  let char = '';
+  let index = 0;
+
+  for (let i = 1; i < string.length; i++) {
+    let repeat = false;
+    if (string[index] !== string[i]) {
+      
     }
   }
 
-  return count;
+  return char;
 }
-
-vowelsNum('AAAAUUAUAUAUAUUAUAUAUA')
-
-const palindromeCheck = (str: string): boolean => {
-  let reversed = str.split('').reverse().join('')
-
-  if (reversed === str) {
-    return true;
-  }
-
-  return false;
-}
-
-palindromeCheck('racecar')
