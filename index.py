894bcf6d27356eee8a9bc859d422b28e29c32dfa
Ridercogswell <ridercogswell@gmail.com>
2022-12-05 15:37:23 -0800

    twoSum

--- index.py
+++ index.py
@@ -7,3 +7,9 @@ def countingSort(arr):
   print(count)
 
 countingSort([ 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ])
+
+def twoSum(nums, target):
+  for i in range(len(nums)):
+    for j in range(i+1, len(nums)):
+      if nums[i] + nums[j] == target:
+        return [i, j]
