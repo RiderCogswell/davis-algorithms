56227680f823566e9785732770131d8a8bbd1e90
Ridercogswell <ridercogswell@gmail.com>
2022-12-12 13:43:44 -0800

    go func

--- maps.go
+++ maps.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	twoSum([]int{2, 11, 7, 15}, 9)
+	threeSum([]int{2, 11, 7, 15, 14, 5, 32, 3, -26}, 9)
 }
 
 func twoSum(nums []int, target int) []int {
@@ -31,3 +32,27 @@ func twoSum(nums []int, target int) []int {
 	return []int{}
 }
 
+func threeSum(nums []int, target int) []int {
+	// Write your code here
+	if len(nums) < 3 {
+		return []int{}
+	}
+
+	// Create a map to store the values
+	// and their indices
+	m := make(map[int]int)
+
+	// Loop through the array
+	for i, v := range nums {
+		// Check if the target minus the current value
+		// is in the map
+		if j, ok := m[target-v]; ok {
+			// If it is, return the indices
+			fmt.Println([]int{j, i})
+		}
+		// Otherwise, add the value and its index
+		// to the map
+		m[v] = i
+	}
+	return []int{}
+}
