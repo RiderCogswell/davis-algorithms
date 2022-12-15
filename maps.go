4736467d33726c98449364f700141a43e8d7d734
Ridercogswell <ridercogswell@gmail.com>
2022-12-15 10:44:22 -0800

    threesum is wrong

--- maps.go
+++ maps.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	twoSum([]int{2, 11, 7, 15}, 9)
-	threeSum([]int{2, 11, 7, 15, 14, 5, 32, 3, -26}, 9)
+	// twoSum([]int{2, 11, 7, 15}, 9)
+	threeSum([]int{2, 11, 7, 15, 14, 5, 32, 3, -26}, 18)
 }
 
 func twoSum(nums []int, target int) []int {
@@ -33,26 +33,5 @@ func twoSum(nums []int, target int) []int {
 }
 
 func threeSum(nums []int, target int) []int {
-	// Write your code here
-	if len(nums) < 3 {
-		return []int{}
-	}
-
-	// Create a map to store the values
-	// and their indices
-	m := make(map[int]int)
-
-	// Loop through the array
-	for i, v := range nums {
-		// Check if the target minus the current value
-		// is in the map
-		if j, ok := m[target-v]; ok {
-			// If it is, return the indices
-			fmt.Println([]int{j, i})
-		}
-		// Otherwise, add the value and its index
-		// to the map
-		m[v] = i
-	}
-	return []int{}
+	
 }
