ad05da3c37d44121fb8fa53c3d1e65f43e0da8e6
Ridercogswell <ridercogswell@gmail.com>
2023-01-11 11:22:49 -0800

    merge sort

--- maps.go
+++ maps.go
@@ -93,3 +93,41 @@ func nThFib(n int) int {
 	return second
 }
 
+func mergeSort(array []int) []int {
+	if len(array) == 1 {
+		return array
+	}
+	mid := len(array) / 2
+	left := mergeSort(array[:mid])
+	right := mergeSort(array[mid:])
+	return merge(mergeSort(left), mergeSort(right))
+}
+
+func merge(left, right []int) (result []int) {
+	result := make([]int, len(left) + len(right))
+
+	i := 0
+
+	for len(left) > 0 && len(right) > 0 {
+		if left[0] < right[0] {
+			result[i] = left[0]
+			left = left[1:]
+		} else {
+			result[i] = right[0]
+			right = right[1:]
+		}
+		i++
+	}
+
+	for j := 0; j < len(left); j++ {
+		result[i] = left[j]
+		i++
+	}
+	
+	for j := 0; j < len(right); j++ {
+		result[i] = right[j]
+		i++
+	}
+	
+	return
+}
