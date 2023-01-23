ab1fc4a901e98241d76f94d1499fae245847961f
Ridercogswell <ridercogswell@gmail.com>
2023-01-23 00:04:27 -0800

    nth fib it  ts

--- index.ts
+++ index.ts
@@ -15,3 +15,13 @@ const nthFib = (n: number): number => {
 
   return nthFib(n - 1) + nthFib(n - 2);
 }
+
+const nthFibIteratively = (n: number): number => {
+  let base = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    base.push(base[i - 2] + base[i - 1]);    
+  }
+
+  return base[n];
+}
