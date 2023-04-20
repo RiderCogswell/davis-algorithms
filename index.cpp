e1dbdaa1b139771b42d6816ef78bfa9a7f5ecd24
Ridercogswell <ridercogswell@gmail.com>
2023-04-20 15:45:48 -0700

    cpp play

--- index.cpp
+++ index.cpp
@@ -9,6 +9,20 @@ using namespace std;
 
 
 // twoSum function
+// explain this line for me
+// vector<int> twoSum(vector<int>& nums, int target) {  // vector<int> is the return type
+//     unordered_map<int, int> map;                    // unordered_map is a data structure
+//     vector<int> result;                             // vector<int> is a data structure
+//     for (int i = 0; i < nums.size(); i++) {         // for loop
+//         int numToFind = target - nums[i];           // int is a data type
+//         if (map.find(numToFind) != map.end()) {     // if statement
+//             result.push_back(map[numToFind]);       // push_back is a vector function
+//             result.push_back(i);                    // push_back is a vector function
+//             return result;                          // return is a function
+//         }
+//         map[nums[i]] = i;                           // map is a data structure
+//     }
+
 vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int, int> map;
     vector<int> result;
